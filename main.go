13deab56fad139014ec0d0a810d04c15e7279664
Shubham Bind <2shubhambind@gmail.com>
2026-04-09 11:39:36 +0530

    working

--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/Versus04/grab-pic/FileUploader"
 	"github.com/Versus04/grab-pic/aws/detection"
 	_ "github.com/aws/aws-sdk-go-v2/config"
 )
 
 func main() {
-	detection.CreateCollection()
-	http.HandleFunc("/upload", FileUploader.FileUploader)
+	detection.ListCollections()
+	//mux := http.NewServeMux()
+	//detection.CreateCollection()
+	//mux.HandleFunc("/upload", FileUploader.FileUploader)
 
-	http.HandleFunc("/getphoto", detection.UserPhoto)
-	http.ListenAndServe("localhost:8080", nil)
+	//mux.HandleFunc("/getphoto", detection.UserPhoto)
+	//http.ListenAndServe(":8080", corsMiddleware(mux))
 
 }
